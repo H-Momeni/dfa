a680ed7cc2bdf3bab15ce894434bb2bc6d4c3ed2
hannaneh <hannan.mgh@gmail.com>
2023-06-11 01:13:05 +0330

    Changes to receive two regex

--- RegexToDFA/RegexToDfa_By_SyntaxTree/src/regextodfa/RegexToDfa.java
+++ RegexToDFA/RegexToDfa_By_SyntaxTree/src/regextodfa/RegexToDfa.java
@@ -9,27 +9,21 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 
-/**
- *
- * @author @ALIREZA_KAY
- */
 public class RegexToDfa {
 
     private static Set<Integer>[] followPos;
     private static Node root;
     private static Set<State> DStates;
-
     private static Set<String> input; // set of characters is used in input regex
 
-    /**
-     * a number is assigned to each characters (even duplicate ones)
-     *
-     * @param symbNum is a hash map has a key which mentions the number and has
-     *                a value which mentions the corresponding character or
-     *                sometimes a string
-     *                for characters is followed up by backslash like "\*"
-     */
+    private static Set<Integer>[] followPos2;
+    private static Node root2;
+    private static Set<State> DStates2;
+    private static Set<String> input2;
+
     private static HashMap<Integer, String> symbNum;
+
+    private static HashMap<Integer, String> symbNum2;
 
     public static void main(String[] args) {
         initialize();
@@ -41,55 +35,66 @@ public class RegexToDfa {
         DStates = new HashSet<>();
         input = new HashSet<String>();
 
+        DStates2 = new HashSet<>();
+        input2 = new HashSet<String>();
+
         String regex = getRegex(in);
         String regex2 = getRegex(in);
 
         getSymbols(regex);
+        getSymbols2(regex2);
 
         /**
          * giving the regex to SyntaxTree class constructor and creating the
          * syntax tree of the regular expression in it
          */
         SyntaxTree st = new SyntaxTree(regex);
+        SyntaxTree st2 = new SyntaxTree(regex2);
+
         root = st.getRoot(); // root of the syntax tree
+        root2 = st2.getRoot(); // root of the syntax tree
 
-        st.printSyntaxTree(st.getRoot());
-        System.out.println(" ");
+        // st2.printSyntaxTree(st2.getRoot());
+        // System.out.println(" ");
         followPos = st.getFollowPos(); // the followpos of the syntax tree
+        followPos2 = st2.getFollowPos(); // the followpos of the syntax tree
 
-        /**
-         * creating the DFA using the syntax tree were created upside and
-         * returning the start state of the resulted DFA
-         */
         State q0 = createDFA();
+        State p0 = createDFA2();
+
         DfaTraversal dfat = new DfaTraversal(q0, input);
+        DfaTraversal dfat2 = new DfaTraversal(p0, input2);
 
         q0.printDFA();
-        String[][] transitionQ = convertStatesToArray(q0);
-      //  System.out.println(transitionQ[0].length);
-        for (int i = 0; i < transitionQ.length; i++) {
-            System.out.print("\nq" + i + "->" );
-            for (int j = 0; j < transitionQ[0].length; j++) {
-                System.out.print( transitionQ[i][j] + " ,");
-            }
-        }
+        p0.printDFA();
 
-        String str = getStr(in);
-        boolean acc = false;
-        for (char c : str.toCharArray()) {
-            if (dfat.setCharacter(c)) {
-                acc = dfat.traverse();
-            } else {
-                System.out.println("WRONG CHARACTER!");
-                System.exit(0);
-            }
-        }
-        if (acc) {
-            System.out.println((char) 27 + "[32m" + "this string is acceptable by the regex!");
-        } else {
-            System.out.println((char) 27 + "[31m" + "this string is not acceptable by the regex!");
-        }
-        in.close();
+        // String[][] transitionQ = convertStatesToArray(q0);
+        // // System.out.println(transitionQ[0].length);
+        // for (int i = 0; i < transitionQ.length; i++) {
+        //     System.out.print("\nq" + i + "->");
+        //     for (int j = 0; j < transitionQ[0].length; j++) {
+        //         System.out.print(transitionQ[i][j] + " ,");
+        //     }
+        // }
+
+        // String str = getStr(in);
+        // boolean acc = false;
+        // for (char c : str.toCharArray()) {
+        // if (dfat.setCharacter(c)) {
+        // acc = dfat.traverse();
+        // } else {
+        // System.out.println("WRONG CHARACTER!");
+        // System.exit(0);
+        // }
+        // }
+        // if (acc) {
+        // System.out.println((char) 27 + "[32m" + "this string is acceptable by the
+        // regex!");
+        // } else {
+        // System.out.println((char) 27 + "[31m" + "this string is not acceptable by the
+        // regex!");
+        // }
+        // in.close();
     }
 
     private static String getRegex(Scanner in) {
@@ -127,6 +132,38 @@ public class RegexToDfa {
             } else {
                 input.add("" + charAt);
                 symbNum.put(num++, "" + charAt);
+            }
+        }
+    }
+
+    private static void getSymbols2(String regex) {
+        /**
+         * op is a set of characters have operational meaning for example '*'
+         * could be a closure operator
+         */
+        Set<Character> op = new HashSet<>();
+        Character[] ch = { '(', ')', '?', '*', '|', '&', '.', '\\', '[', ']', '+' };
+        op.addAll(Arrays.asList(ch));
+
+        input2 = new HashSet<>();
+        symbNum2 = new HashMap<>();
+        int num = 1;
+        for (int i = 0; i < regex.length(); i++) {
+            char charAt = regex.charAt(i);
+
+            /**
+             * if a character which is also an operator, is followed up by
+             * backslash ('\'), then we should consider it as a normal character
+             * and not an operator
+             */
+            if (op.contains(charAt)) {
+                if (i - 1 >= 0 && regex.charAt(i - 1) == '\\') {
+                    input2.add("\\" + charAt);
+                    symbNum2.put(num++, "\\" + charAt);
+                }
+            } else {
+                input2.add("" + charAt);
+                symbNum2.put(num++, "" + charAt);
             }
         }
     }
@@ -185,6 +222,69 @@ public class RegexToDfa {
                         q.setAccept();
                     }
                     DStates.add(q);
+                    tmp = q;
+                }
+                s.addMove(a, tmp);
+            }
+        }
+
+        return q0;
+    }
+
+    private static State createDFA2() {
+        int id = 0;
+        Set<Integer> firstpos_n0 = root2.getFirstPos();
+
+        State q0 = new State(id++);
+        q0.addAllToName(firstpos_n0);
+        if (q0.getName().contains(followPos2.length)) {
+            q0.setAccept();
+        }
+        DStates2.clear();
+        DStates2.add(q0);
+
+        while (true) {
+            boolean exit = true;
+            State s = null;
+            for (State state : DStates2) {
+                if (!state.getIsMarked()) {
+                    exit = false;
+                    s = state;
+                }
+            }
+            if (exit) {
+                break;
+            }
+
+            if (s.getIsMarked()) {
+                continue;
+            }
+            s.setIsMarked(true); // mark the state
+            Set<Integer> name = s.getName();
+            for (String a : input2) {
+                Set<Integer> U = new HashSet<>();
+                for (int p : name) {
+                    if (symbNum2.get(p).equals(a)) {
+                        U.addAll(followPos2[p - 1]);
+                    }
+                }
+
+                boolean flag = false;
+                State tmp = null;
+                for (State state : DStates2) {
+                    if (state.getName().equals(U)) {
+                        tmp = state;
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag) {
+                    State q = new State(id++);
+                    q.addAllToName(U);
+                    if (U.contains(followPos2.length)) {
+                        q.setAccept();
+                    }
+                    DStates2.add(q);
                     tmp = q;
                 }
                 s.addMove(a, tmp);
